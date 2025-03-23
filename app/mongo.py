fdae6f519e88473141857e9aca2222dd578066c9
csvminer <mendieta.eduardo@outlook.com>
2025-03-23 11:47:47 -0500

    Agregando archivos originales de pf-s2.

--- app/mongo.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-def conectar_db():
-    mongo_base = MongoClient('mongodb://localhost:27017/')
-    db = mongo_base['productodb']
-    coleccion = db['productos']
-    return mongo_base, db, coleccion
--- app/procesos.py
+++
@@ -1,9 +0,0 @@
-
-def presentar_menu():
-    print('\n-------- MENÚ --------')
-    print('1) Nuevo producto.')
-    print('2) Listar productos.')
-    print('3) Editar producto.')
-    print('4) Buscar producto.')
-    print('5) Eliminar producto.')
-    print('Otro) Salir.')
